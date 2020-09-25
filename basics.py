fcbb6e2b95faab585ce04a031999356b371b0c31
Artem Shakurov <galavasteg@gmail.com>
2020-09-25 10:42:25 +0300

    tasks on python basics: decorator

---
+++ basics.py
@@ -0,0 +1,36 @@
+# 1. Write a decorator not allowing the function to be called more than
+#  a certain number of times (parameter).
+
+
+def raise_exception_if_called_too_many_times(function, call_count_threshold: int):
+    """
+    Decorator that count function calls and raise UnboundLocalError exception
+    on more than *call_count_threshold* call.
+
+    :param function: any callable function.
+    :param call_count_threshold: allowed maximum number of function calls.
+    :return: decorated function
+
+    >>> threshold = 1
+    >>> do_not_call_twice = raise_exception_if_called_too_many_times(
+    ...     lambda: 1, threshold)
+    >>> do_not_call_twice()
+    1
+    >>> do_not_call_twice()
+    Traceback (most recent call last):
+      File ..., ..., in decorated
+        raise UnboundLocalError(msg)
+    UnboundLocalError: Maximum number of function calls exceeded: 1
+
+    """
+    def decorated(*args, **kwargs):
+        if decorated._call_count == call_count_threshold:
+            msg = 'Maximum number of function calls exceeded: {c}'.format(
+                    c=call_count_threshold)
+            raise UnboundLocalError(msg)
+
+        decorated._call_count = decorated._call_count + 1
+        return function(*args, **kwargs)
+
+    decorated._call_count = 0
+    return decorated
